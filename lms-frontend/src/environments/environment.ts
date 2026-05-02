a0c7fe1a752c79e9a056c22f589e89ce347ce8d8
Skerdi199 <skerdivelia@gmail.com>
2026-05-02 14:26:49 +0200

    connect monorepo confing

--- lms-frontend/src/environments/environment.ts
+++ lms-frontend/src/environments/environment.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api',
+//   stripePublicKey: 'pk_test_your_stripe_public_key'
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
+  production: true,
+  apiUrl: 'https://lms-monorepo-1.onrender.com/api',
   stripePublicKey: 'pk_test_your_stripe_public_key'
 };
